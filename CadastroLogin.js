0ee72f842847f81c818608d0298bcf26508993c0
Victor Laves <victorpf2020@gmail.com>
2022-10-25 22:34:28 -0300

    Exercicio 2 Opções de cadastro

--- CadastroLogin.js
+++ CadastroLogin.js
@@ -1,5 +1,6 @@
 var iName = 0
 var iPassword = 0
+var close = true
 var arrayName = []
 var arrayPassword = []
 
@@ -11,4 +12,19 @@ function NameAndPassword(){
     
     arrayName[iName] = name
     arrayPassword[iPassword] = password
+    arrayName++
+    arrayPassword++
+}
+
+function ChooseOption(){
+    var option = prompt("Escolha a opção que deseja... 1 = Cadastrar, 2 = Login, 3 = Excluir cadastro, 4 = Encerrar programa")
+    if(option == 1)
+    NameAndPassword()
+    else if(option == 2)
+    Login()
+    else if(option == 3)
+    Excluir()
+    else{
+        close = false
+    }
 }
